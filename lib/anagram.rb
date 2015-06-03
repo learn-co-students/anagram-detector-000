fc42686422d3380f16a3e88ecde4dccb94a932b3
gjc2118 <gjc2118@gmail.com>
2015-06-02 18:39:48 -0700

    My first anagram matcher

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+
+def initialize(word)
+  @anagram = word
+end
+
+def match(array)
+  returnarray = []
+  array.each{|word|
+    anagram = @anagram.chars
+    if word.length == anagram.length then 
+      word.chars.each {|letter| 
+        if anagram.find_index(letter) ==nil then break
+        else anagram.delete_at(anagram.find_index(letter))
+        end}
+      if anagram.length==0 then returnarray << word end
+    end
+  }
+  returnarray
+end
+
+end
