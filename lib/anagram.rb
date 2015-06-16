1faf9bac788ccb5ca5d4357ec71cd060adb803b2
chaplonglau <chaplonglau@gmail.com>
2015-06-16 16:43:58 -0700

    cool

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
 # Your code goes here!
+
+
+class Anagram
+
+  attr_accessor :Anagram, :original, :collection
+
+  def initialize(anagram)
+    @Anagram=anagram
+  end
+
+  def match(array)
+   
+    @Anagram=@Anagram.downcase
+    @Anagram=@Anagram.chars.sort{|a,b| a.casecmp(b)}.join
+    puts @Anagram
+
+    @collection=[]
+
+    array.each do |x|
+      @original=x
+      x=x.chars.sort{|a,b| a.casecmp(b)}.join
+   
+      if x==@Anagram
+     
+        @collection<<@original
+      end
+    end
+    
+    return @collection
+
+  end
+
+end
