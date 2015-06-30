2848eaaf0bac0e78c81b7c65f129f2880e23a4f2
aeremin <aeremin@maxus.ru>
2015-06-30 14:06:26 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  def initialize(str)
+    @source = str
+  end
+ 
+  def match(array)
+    #returns array of matching
+	sorted_source = @source.split(//).sort.join
+	array.each_with_object([]) do |str, res|
+	  sorted_str = str.split(//).sort.join
+	  res << str if sorted_source == sorted_str
+	end
+  end  
+end
