e2be25f3965e40cc7f3e56fcf67b34cf65595913
Rigz777 <rigzgoodmusic@gmail.com>
2015-09-19 00:29:40 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_array)
+    arr = []
+    chosen_word = @word.split(//).sort
+    anagram_array.each do |words|
+      possible_anagram = words.split(//).sort
+      if possible_anagram == chosen_word
+        arr << words
+      end
+    end
+    arr
+  end
+end
