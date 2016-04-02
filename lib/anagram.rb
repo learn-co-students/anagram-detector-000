1f382d4f46f3915a68e3b6ebef14843708fd5d32
mendyismyname <m.seldowitz@gmail.com>
2016-04-02 19:04:16 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    result = array.collect do |word|
+      word if word.split("").sort == @word.split("").sort
+    end
+    result.compact
+  end 
+end
