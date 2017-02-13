e1689edc6f65142ebca2f5188e365dfae0c75b0d
pkprosol <peter.prosol@gmail.com>
2017-02-13 15:23:40 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        return_array = []
+        anagrams.each do |anagram|
+            if anagram.split("").sort == @word.split("").sort
+                return_array << anagram
+            end
+        end
+        return_array
+    end
+end
