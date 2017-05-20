5b71f59b2bcd68c169952527189db79a87bfebda
OnlyHughMan <hughvidler@hotmail.com>
2017-05-20 22:49:16 +1000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_reader :anagram
+  
+  def initialize(anagram)
+    @anagram = anagram
+  end
+  
+  def match(words)
+    words.select { |word| (word.downcase == anagram) ? false : anagram?(word) }
+  end
+  
+  def anagram?(word)
+    word.downcase.split('').sort == anagram.downcase.split('').sort
+  end
+end
